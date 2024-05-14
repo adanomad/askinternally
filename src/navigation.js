8f14226ab730cf9fd39f2134a2dd5a9f31a47a2e
jonathanAdanomad <jonathan@adanomad.com>
2024-05-14 16:16:51 -0400

    Fixed comments from last PR

--- src/navigation.js
+++ src/navigation.js
@@ -57,7 +57,7 @@ export const headerData = {
     //   ],
     // },
     {
-      text: 'Help',
+      text: 'Help',href: '#help',
       links: [
         // {
         //   text: 'Lead Generation',
@@ -86,7 +86,7 @@ export const headerData = {
       ],
     },
     {
-      text: 'FAQs',
+      text: 'FAQs',href:'#faq',
       links: [
         // {
         //   text: 'Blog List',
@@ -111,7 +111,7 @@ export const headerData = {
       ],
     },
   ],
-  actions: [{ text: 'Sign In', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  actions: [{ text: 'Sign In', href: '', target: '_blank' }],
 };
 
 export const footerData = {
@@ -166,14 +166,8 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: ''},
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/adanomad/'},
   ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
+  
 };
